cf84558e1bebe79dce1e89cd086407127fddaa6d
tyage <namatyage@gmail.com>
2016-10-27 16:39:16 +0900

    top page

--- index.js
+++ index.js
@@ -12,16 +12,21 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
+  const code = request.query.code;
+  if (code === undefined) {
+    return response.render('pages/index', {
+      clientId: process.env.SLACK_CLIENT_ID
+    });
+  }
+
   client.oauth
-    .access(process.env.SLACK_CLIENT_ID, process.env.SLACK_CLIENT_SECRET, request.query.code)
+    .access(process.env.SLACK_CLIENT_ID, process.env.SLACK_CLIENT_SECRET, code)
     .then(res => {
-      response.render('pages/index', {
+      response.render('pages/accessToken', {
         accessToken: res.access_token
       });
     }).catch(err => {
-      response.render('pages/index', {
-        accessToken: null
-      });
+      response.render('pages/error');
     });
 });
 
