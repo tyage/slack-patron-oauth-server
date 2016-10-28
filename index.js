2345f623a55b7c64089a1698bd87c63e5b910999
tyage <namatyage@gmail.com>
2016-10-28 16:09:59 +0900

    client scope is needed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const WebClient = require('@slack/client').WebClient;
 const client = new WebClient();
 
-const permissions = 'channels:history groups:history im:history team:read users:read channels:read groups:read im:read emoji:read';
+const permissions = 'client';
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,7 +18,7 @@ app.get('/', (request, response) => {
   if (code === undefined) {
     return response.render('pages/index', {
       clientId: process.env.SLACK_CLIENT_ID,
-      scope: permissions
+      permissions: permissions
     });
   }
 
