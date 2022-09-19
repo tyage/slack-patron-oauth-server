06bbf2388c01598e46ea2c1090338d7778294306
tyage <namatyage@gmail.com>
2022-09-19 20:38:05 +0900

    styling

--- pages/callback.tsx
+++ pages/callback.tsx
@@ -1,34 +1,50 @@
-import type { GetServerSideProps, NextPage } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
-import { WebClient } from '@slack/web-api'
+import type { GetServerSideProps, NextPage } from "next";
+import { Container, Link, Card, Text } from "@nextui-org/react";
+import { WebClient } from "@slack/web-api";
 
 type CallbackProp = {
-  accessToken: string
-}
+  accessToken: string;
+};
 
 const Home: NextPage<CallbackProp> = ({ accessToken }) => {
   return (
-    <div>
-      {accessToken}
-    </div>
-  )
-}
+    <Container lg>
+      <Text h1>Run Slack Patron!</Text>
 
-export const getServerSideProps: GetServerSideProps<CallbackProp> = async ({ query }) => {
-  const { code } = query
+      <Card variant="flat">
+        <Card.Body>
+          <pre>
+            {`$ wget https://raw.githubusercontent.com/tyage/slack-patron/master/docs/docker-compose-quick.yml
+$ export SLACK_PATRON_SLACK_TOKEN=${accessToken}
+$ docker compose -f ./docker-compose-quick.yml up
+$ open http://localhost:9292`}
+          </pre>
+        </Card.Body>
+      </Card>
 
-  const res = await (new WebClient()).oauth.v2.access({
+      <Link target="_blank" href="https://github.com/tyage/slack-patron">
+        Visit source code on GitHub.
+      </Link>
+    </Container>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<CallbackProp> = async ({
+  query,
+}) => {
+  const { code } = query;
+
+  const res = await new WebClient().oauth.v2.access({
     client_id: process.env.SLACK_CLIENT_ID || "",
     client_secret: process.env.SLACK_CLIENT_SECRET || "",
-    code: String(code)
-  })
+    code: String(code),
+  });
 
   return {
     props: {
-      accessToken: res.access_token || ""
-    }
-  }
-}
+      accessToken: res.access_token || "",
+    },
+  };
+};
 
-export default Home
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,46 @@
-import type { GetStaticProps, NextPage } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import { env } from 'process'
+import type { GetStaticProps, NextPage } from "next";
+import { Container, Link, Image, Text } from "@nextui-org/react";
+import { env } from "process";
 
 type HomeProp = {
-  clientId: string
-}
+  clientId: string;
+};
 
 const Home: NextPage<HomeProp, {}> = ({ clientId }) => {
-  const permissions = 'channels:history,team:read'
-  const link = `https://slack.com/oauth/v2/authorize?client_id=${encodeURIComponent(clientId)}&scope=${encodeURIComponent(permissions)}`
+  const permissions = "channels:history,team:read";
+  const link = `https://slack.com/oauth/v2/authorize?client_id=${encodeURIComponent(
+    clientId
+  )}&scope=${encodeURIComponent(permissions)}`;
 
   return (
-    <div className={styles.container}>
+    <Container
+      justify="center"
+      alignContent="center"
+      alignItems="center"
+      display="flex"
+      direction="column"
+      responsive
+      style={{ height: "100vh" }}
+    >
+      <Text h2>Install Slack Patron?</Text>
       <Link href={link}>
-       <Image alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" />
+        <Image
+          alt="Add to Slack"
+          height="40"
+          width="139"
+          src="https://platform.slack-edge.com/img/add_to_slack.png"
+        />
       </Link>
-    </div>
-  )
-}
+    </Container>
+  );
+};
 
 export const getStaticProps: GetStaticProps<HomeProp> = () => {
   return {
     props: {
-      clientId: env.SLACK_CLIENT_ID || ""
-    }
-  }
-}
+      clientId: env.SLACK_CLIENT_ID || "",
+    },
+  };
+};
 
-export default Home
+export default Home;
